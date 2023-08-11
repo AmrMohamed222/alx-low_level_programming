412a9236ce3a4f6da38f17b54007450b7f1b8dd7
Amr Mohamed <amrm91432@outlook.com>
2023-08-11 12:28:54 -0700

    task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - function for printing alphabet
+ * Return: Alwayas (Success)
+ */
+int main(void)
+{
+	int count = 97;
+	for (count; count <= 122; count++)
+	{
+	putchar(count);
+	}
+	putchar('\n');
+	return (0);
+}
