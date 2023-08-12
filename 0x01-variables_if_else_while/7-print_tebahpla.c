dc7629c6e291d980f9a0d5ab33d4c98a078b04f7
Amr Mohamed <amrm91432@outlook.com>
2023-08-11 17:35:45 -0700

    task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - this is my function
+*
+* Return: 0 when successfull
+*/
+
+int main(void)
+{
+	char l;
+
+	for (l = 'z'; l >= 'a'; l--)
+			putchar(l);
+		putchar('\n');
+
+	return (0);
+}
