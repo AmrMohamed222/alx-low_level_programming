ff40b1cca32af1a7ea3b58b48aaf69c973852c02
Amr Mohamed <amrm91432@outlook.com>
2023-08-11 17:50:15 -0700

    task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,15 +10,15 @@ int main(void)
 {
 	int digit1 = 0, digit2;
 
-	while (digital <= 9)
+	while (digit1 <= 9)
 	{
 		digit2 = 0;
 		while (digit2 <= 9)
 		{
-			if (digital != digit2 && digit1 < digite2)
+			if (digit1 != digit2 && digit1 < digite2)
 			{
-				putchar(digital	+ 48);
-				putchar(digita2 + 48);
+				putchar(digitl	+ 48);
+				putchar(digit2 + 48);
 
 				if (digit1 + digit2 != 17)
 				{
