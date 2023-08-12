fdcdf3ee09897082a323ca639f2cfa566bf1976e
Amr Mohamed <amrm91432@outlook.com>
2023-08-11 17:47:58 -0700

    task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+* main - this is my function
+*
+* Return: 0 when successfull
+*/
+
+int main(void)
+{
+	int digit1 = 0, digit2;
+
+	while (digital <= 9)
+	{
+		digit2 = 0;
+		while (digit2 <= 9)
+		{
+			if (digital != digit2 && digit1 < digite2)
+			{
+				putchar(digital	+ 48);
+				putchar(digita2 + 48);
+
+				if (digit1 + digit2 != 17)
+				{
+					putchar(' ');
+					putchar(' ');
+				}
+			}
+			digit2++;
+		}
+		digit1++;
+	}
+	putchar('\n');
+	return (0);
+}
