d0d4bb064c724312ccf590820b4d09dc9d450197
Amr Mohamed <amrm91432@outlook.com>
2023-08-15 14:45:47 -0700

    task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *
+ * add - adding two values
+ *
+ * @i: variable one
+ *
+ * @j: variable two
+ *
+ * Return: summation of the two values
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
