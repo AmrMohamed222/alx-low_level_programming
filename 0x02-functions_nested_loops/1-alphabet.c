0beb8b9df3c57f39b582bd67bd830787f0c877b2
Amr Mohamed <amrm91432@outlook.com>
2023-08-15 14:13:07 -0700

    task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,17 @@
 #include "main.h"
+
 /**
- * print_alphabet - Entry point
- *
- * Description: 'prints alphabet in lower case'
- * 
- * Return: Always (Success)
- */
+* print_alphabet - Entry point
+*
+* Description: 'prints alphabet in lower case'
+*
+* Return: Always (Success)
+*/
+
 void print_alphabet(void)
 {
 	int i = 97;
-	
+
 	for (; i <= 122; i++)
 		_putchar(i);
 	_putchar('\n');
