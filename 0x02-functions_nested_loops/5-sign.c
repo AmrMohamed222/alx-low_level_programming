6ec7b7ec9f6b2bbb3c9c6222c465bca65f36d27a
Amr Mohamed <amrm91432@outlook.com>
2023-08-15 14:31:27 -0700

    task

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - print the sing of the input
+ * @n: the input to be checked
+ * Return: 1 if greater zero 0 if zero -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar(45);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar(43);
+		return (1);
+	}
+}
