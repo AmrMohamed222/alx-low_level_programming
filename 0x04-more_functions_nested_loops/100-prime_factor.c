c776da3b6c8c20312b7933c6d9860af188653370
Amr Mohamed <amrm91432@outlook.com>
2023-08-17 17:28:36 -0700

    task

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entery Point
+ *
+ * description: numbersfactor
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int i = 612852475143, factors;
+
+	for (factors = 2; factors < i; factors++)
+	{
+		if (i % factors == 0)
+			i /= factors;
+	}
+
+	printf("%ld\n", factors);
+	return (0);
+}
