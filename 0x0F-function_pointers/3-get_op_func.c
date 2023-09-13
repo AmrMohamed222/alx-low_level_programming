b65e8a804f36bcaf47c0e9f7e89aba664f83ec46
Amr Mohamed <amrm91432@outlook.com>
2023-09-13 03:18:10 -0700

    task

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -21,13 +21,11 @@ int (*get_op_func(char *s))(int, int)
 	int i;
 
 	i = 0;
-
 	while (ops[i].op)
 	{
-		if (strcmp(ops[i].op, s) == 0)
+		if (ops[i].op[0] == s[0] && s[1] == '\0')
 			return (ops[i].f);
-		i++;
+		++i;
 	}
-
 	return (NULL);
 }
