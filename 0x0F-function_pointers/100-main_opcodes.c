8ebddc4cb0dadacd0f793cb995983190d1b8a18a
Amr Mohamed <amrm91432@outlook.com>
2023-09-13 03:12:08 -0700

    task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,44 +1,34 @@
-#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - prints its own opcodes
+ * main - print the opcode of it own function
  *
- * argc: number of arguments
+ * @argc: number of command line arguments
  *
- * @argv: array of arguments
+ * @argv: array of command line arguments
  *
- * Return: Always 0 (Success)
+ * Return: Exit 1 if one arg is not correct, 2 if byte is negative.
  */
 
 int main(int argc, char *argv[])
 {
-	int bytes, i;
-	char *arr;
+	short bytes, x;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-
 	bytes = atoi(argv[1]);
-
 	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
-
-	arr = (char *)main;
-
-	for (i = 0; i < bytes; i++)
-	{
-		if (i == bytes - 1)
-		{
-			printf("%02hhx\n", arr[i]);
-			break;
-		}
-		printf("%02hhx ", arr[i]);
-	}
-	return (0);
+	printf("%02x", *((unsigned char *) (main)));
+	for (x = 1; x < bytes; ++x)
+		printf(" %02x", *((unsigned char *) (main + x)));
+	putchar('\n');
+	exit(EXIT_SUCCESS);
 }
